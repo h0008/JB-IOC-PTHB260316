1e8218a3bf4e2dda352adfa5b696ddf9f6dabf25
h0008 <duy6902@gmail.com>
2026-05-06 11:25:30 +0700

    Remove BE-101 homeworks; add Session04 Java
    Delete many BE-101 homework assets (images, SQL scripts and a backup) across SS01–SS09 folders. Add a new Session04 Java project under JB-IOC-PTHB260316_IOC-BE/Session04 including .gitignore, IntelliJ project files (.idea/*, .iml) and source files src/Main.java and src/Session04/DescArraySort.java.

---
+++ JB-IOC-PTHB260316_IOC-BE/Session04/src/Session04/DescArraySort.java
@@ -0,0 +1,36 @@
+package Session04;
+
+import java.util.Scanner;
+
+public class DescArraySort {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            arr[i] = scanner.nextInt();
+        }
+        bubbleSort(arr);
+        System.out.println("\nSorted array in descending order:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        scanner.close();
+    }
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
