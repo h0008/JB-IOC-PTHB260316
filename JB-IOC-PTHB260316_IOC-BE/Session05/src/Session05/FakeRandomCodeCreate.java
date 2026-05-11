5af1eabea0659b14c92b56d709f2c8218fed7c82
h0008 <duy6902@gmail.com>
2026-05-11 12:07:33 +0700

    Add random code generator and password validator
    Add two CLI utilities: FakeRandomCodeCreate and ValidPassCheck. FakeRandomCodeCreate reads a length (1-1000) from stdin, generates a random alphanumeric string (A-Z, a-z, 0-9) of that length using java.util.Random, and prints it. ValidPassCheck reads a password from stdin and validates it with a regex requiring at least 8 characters, one uppercase, one lowercase, one digit and one special char from @#$!%; it prints a validation result message.

--- JB-IOC-PTHB260316_IOC-BE/Session05/src/Session05/FakeRandomCodeCreate.java
+++ JB-IOC-PTHB260316_IOC-BE/Session05/src/Session05/FakeRandomCodeCreate.java
@@ -1,4 +1,34 @@
 package Session05;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class FakeRandomCodeCreate {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter length (1-1000): ");
+		int n = 0;
+		if (sc.hasNextInt()) {
+			n = sc.nextInt();
+		}
+		sc.close();
+		if (n < 1 || n > 1000) {
+			return;
+		}
+		Random rnd = new Random();
+		StringBuilder sb = new StringBuilder(n);
+		for (int i = 0; i < n; i++) {
+			int r = rnd.nextInt(62); // 0-61
+			char c;
+			if (r < 26) {
+				c = (char) ('A' + r);
+			} else if (r < 52) {
+				c = (char) ('a' + (r - 26));
+			} else {
+				c = (char) ('0' + (r - 52));
+			}
+			sb.append(c);
+		}
+		System.out.print(sb.toString());
+	}
 }
--- JB-IOC-PTHB260316_IOC-BE/Session05/src/Session05/ValidPassCheck.java
+++ JB-IOC-PTHB260316_IOC-BE/Session05/src/Session05/ValidPassCheck.java
@@ -1,4 +1,21 @@
 package Session05;
 
+import java.util.Scanner;
+
 public class ValidPassCheck {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter password: ");
+		String input = sc.nextLine();
+		sc.close();
+
+		String pwd = input == null ? "" : input.trim();
+		String pattern = "^(?=.{8,}$)(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$!%]).*$";
+		if (pwd.matches(pattern)) {
+			System.out.println("Valid password!");
+		} else {
+			System.out.println("Invalid password...");
+		}
+	}
 }
