10fcab0aba83770d955c66464a36827456c0927e
h0008 <duy6902@gmail.com>
2026-05-20 12:01:52 +0700

    Add Session10 shape and animal classes
    Add Java sources and compiled classes for Session10 exercises. Implements Shape, Rectangle and Circle with overridden area() and includes ShapeExercise.java with a main that prints areas. Also adds an empty OverridingShapeRectangleCircle.java stub and compiled Animal/Dog/AnimalToDogInheritance .class files.

---
+++ JB-IOC-PTHB260316_IOC-BE/Session10/src/Session10/ShapeExercise.java
@@ -0,0 +1,45 @@
+package Session10;
+
+class Shape {
+    public double area() {
+        return 0;
+    }
+}
+
+class Rectangle extends Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+public class ShapeExercise {
+    public static void main(String[] args) {
+        Shape s1 = new Rectangle(2, 3);
+        Shape s2 = new Circle(1);
+        System.out.println("Rectangle area: " + s1.area());
+        System.out.println("Circle area: " + s2.area());
+    }
+}
+
